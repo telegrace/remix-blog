2b519dcddd1b6f0e5a1d098256062ce00158de72
telegrace <telegrace@gmx.de>
2022-04-04 17:08:42 +0200

    feat: blog posts working, but not the link

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,35 @@
+import { json, Link, useLoaderData } from "remix";
+import { getPosts } from "~/models/post.server";
+
+type Post = {
+  slug: string;
+  title: string;
+};
+
+type LoaderData = {
+  posts: Awaited<ReturnType<typeof getPosts>>;
+};
+
+export const loader = async () => {
+  return json<LoaderData>({
+    posts: await getPosts(),
+  });
+};
+
+export default function Posts() {
+  const { posts } = useLoaderData() as LoaderData;
+  return (
+    <main>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post: Post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
--- remix.config.js
+++ remix.config.js
@@ -5,3 +5,5 @@ module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
 };
+
+// this does not run on the server
